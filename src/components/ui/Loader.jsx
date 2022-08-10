944965ce42f538c6f771d1f65e398701ba8f3c5c
System Administrator <root@ltp-61.local>
2022-08-10 22:21:05 +0800

    updates

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const Loader = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="fixed w-full h-full top-0 left-0 z-[999999] bg-white flex items-center justify-center flex-col loader">
+    <div
+      className={`fixed w-full h-full top-0 left-0 z-[999999] bg-white flex items-center justify-center flex-col loader ${
+        !loading ? "fade" : ""
+      }`}
+    >
       <div class="multi-spinner-container z-10 mb-10">
         <div class="multi-spinner">
           <div class="multi-spinner">
