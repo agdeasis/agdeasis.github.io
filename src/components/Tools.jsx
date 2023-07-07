495db6ee72e79c47509017b7c11561945cfec145
agdeasis <arneldeeasis@gmail.com>
2023-07-07 09:46:48 +0800

    update cv

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import ToolsList from "./ui/tools/ToolsList";
 
 const Tools = () => {
-  return (
-    <div className="container">
-      <div className="overflow-x-clip">
-        <h1 className=" header-title">
-          Tools
-          <span>TECHNOLOGIES</span>
-        </h1>
-        <i className="mt-10 block">Techonologies and tools that I use.</i>
-      </div>
-      <div className="mb-12">
-        <ToolsList />
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="overflow-x-clip">
+                <h1 className=" header-title">
+                    Tools
+                    <span>TECHNOLOGIES</span>
+                </h1>
+                <i className="mt-10 block">
+                    Technologies and tools that I use.
+                </i>
+            </div>
+            <div className="mb-12">
+                <ToolsList />
+            </div>
+        </div>
+    );
 };
 
 export default Tools;
